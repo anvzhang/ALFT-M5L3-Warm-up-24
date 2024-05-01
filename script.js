5f21a4a7970c11ea1c0dfca8e7bece14f5cbf3aa
anvzhang <165081556+anvzhang@users.noreply.github.com>
2024-05-01 19:56:13 +0000

    done

---
+++ script.js
@@ -0,0 +1,13 @@
+let mood = "happy";
+
+let recommendedSong;
+
+if (mood === "happy") {
+  recommendedSong = "Here Comes the Sun by The Beatles";
+} else if (mood === "sad") {
+  recommendedSong = "Someone Like You by Adele";
+} else {
+  recommendedSong = "Break My Soul by Beyonce";
+}
+
+console.log(recommendedSong);
